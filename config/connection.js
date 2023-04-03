2946e19a60b9acb19aefa91c0467ed4fac46502d
Hunter Wilson <huntertheguy123@gmail.com>
2023-04-03 13:32:13 -0500

    I added to the connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,17 @@
-const sequilize = require('sequilize');
+const Sequilize = require('sequilize');
 
 require('dotenv').config();
+
+const sequelize = new Sequilize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306,
+    }
+);
+
+module.exports = sequelize;
 
