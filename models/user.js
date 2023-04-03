17cfb36f4a7a2dbb982e801df6c3cb7d73cbca8d
Hunter Wilson <huntertheguy123@gmail.com>
2023-04-03 13:27:40 -0500

    I added the necessary npm packages as well as the user model

---
+++ models/user.js
@@ -0,0 +1,47 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class User extends Model {
+    checkPassword(loginPass) {
+        return bcrypt.compareSync(loginPass, this.password);
+    }
+}
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        password: {
+           type: DataTypes.STRING,
+           allowNull: false,
+           validate: {
+            len: [6, 20],
+            notEmpty: true,
+           }, 
+        },
+    },
+    {
+        hooks: {
+            async beforeCreate(newUser) {
+                newUser.password = await bcrypt.hash(newUser.password, 10);
+                return newUser
+            },
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user',
+    }
+);
+
+module.exports = User;
