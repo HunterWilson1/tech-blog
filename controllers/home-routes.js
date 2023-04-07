5f7d6567153115d38ee10100f4363d134d6dfecb
Hunter Wilson <huntertheguy123@gmail.com>
2023-04-06 20:54:41 -0500

    I added to the home-routes.js. Not finished

---
+++ controllers/home-routes.js
@@ -0,0 +1,39 @@
+const router = require('express').Router();
+const {Post, User, Comment } = require('../models');
+
+router.get('/', (req, res) => {
+    Post.findAll({
+        attributes: [
+            'id',
+            'title',
+            'content', 
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: [
+                    'id',
+                    'comment_text',
+                    'post_id',
+                    'user_id',
+                ],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            }
+        ]
+    })
+    .then(dbPostData => {
+        const posts = dbPostData.map(psot => post.get({ plain: true }));
+
+        res.render('homepage', {
+            posts,
+            loggedIn: req.session.loggedIn
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
