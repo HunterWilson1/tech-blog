321bc7b696c7109b4f2a4899a0d3bdc142bca9f1
Hunter Wilson <huntertheguy123@gmail.com>
2023-04-12 13:43:04 -0500

    added some files and added more to home-routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -37,3 +37,55 @@ router.get('/', (req, res) => {
         res.status(500).json(err);
     });
 });
+
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'title',
+            'content',
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: [
+                    'id',
+                    'comment_text',
+                    'post_id',
+                    'user_id'
+                ],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No post found'});
+            return;
+        }
+    })
+});
+
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn){
+        res.redirect('/');
+        return;
+    }
+    res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+    res.render('signup');
+});
+
+module.exports = router
